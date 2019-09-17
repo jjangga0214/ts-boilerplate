a4afebb8c92c092bbcd967814e264250ed43c461
GIL B. Chan <bnbcmindnpass@gmail.com>
2019-09-17 10:31:43 +0900

    fix(dist): remove tests from compiled code

--- jest.config.js
+++ jest.config.js
@@ -61,7 +61,7 @@ module.exports = {
   // ],
 
   // An array of file extensions your modules use
-  moduleFileExtensions: ['js', 'json', 'jsx', 'ts', 'tsx', 'node'],
+  moduleFileExtensions: ['js', 'jsx', 'ts', 'tsx'],
 
   // A map from regular expressions to module names that allow to stub out resources with a single module
   // moduleNameMapper: {},
